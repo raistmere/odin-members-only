eb517a8592b9281850cb9a1d6f9dfd0f13803647
raistmere <raistmere@gmail.com>
2024-05-04 11:16:21 -0500

    feat: added new route to sign-up + sign-up form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET sign up form
+router.get("/signup", function(req, res, next) {
+  res.render("signup_form", {
+    title: "Sign-up form"
+  })
+});
+
 module.exports = router;
